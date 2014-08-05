3486c3c277287f2b27c021d798d1bd0f2eca3ce0
diallobirrou <diallobirrou@gmail.com>
2014-08-05 11:10:59 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ def new
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
